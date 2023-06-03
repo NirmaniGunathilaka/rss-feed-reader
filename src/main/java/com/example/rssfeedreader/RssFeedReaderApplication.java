c429ec2da48cad563117be6e99c1e46a383c122f
Nirmani <nirmani7678@gmail.com>
2023-06-04 00:26:10 +0530

    1. Configuring h2 database 2. Enabling swagger ui

--- src/main/java/com/example/rssfeedreader/RssFeedReaderApplication.java
+++ src/main/java/com/example/rssfeedreader/RssFeedReaderApplication.java
@@ -2,12 +2,23 @@ package com.example.rssfeedreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
+@Configuration
 public class RssFeedReaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RssFeedReaderApplication.class, args);
 	}
 
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.rssfeedreader")).build();
+	}
 }
